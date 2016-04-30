a9ff7ef994b52d569f9c39730b158ad129bd81f6
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-04-30 13:53:38 -0300

    update: scripts

--- source/assets/js/script.js
+++ source/assets/js/script.js
@@ -1,18 +1,47 @@
-$(document).ready(function(){
+$(function(){
 
   var map;
-  var markerArr=[];
+  var markerArr = [];
 
   function initialize() {
     var mapOptions = {
       zoom: 9,
       mapTypeControl: false,
-      center: new google.maps.LatLng(42.36, -71.06)
+      center: new google.maps.LatLng(-22.906847, -43.172896)
     };
-    map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-    // image from external URL
-    var myIcon ='https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/cheshire1-icon.png';
+    var features = [
+      { "type": "Feature", "properties": {
+        "Company": "Boston Scientific",
+        "StoreID": null,
+        "Site_Store_Facility Name": "Boston Scientific - Quincy Location",
+        "Type of Facility": null,
+        "Fac_simple": null,
+        "Street Address": "500 Commander Shea Boulevard",
+        "City": "Quincy",
+        "State": "MA",
+        "Zip": "2171",
+        "OnlineDate": null,
+        "Capacity": "1200",
+        "System Mounting": null,
+        "NEM": null,
+        "Solar Installer Company": null,
+        "Solar System Owner": null,
+        "Facility Owner": null,
+        "Facility Tenant": null,
+        "Notes": null,
+        "New": 0,
+        "LocationGeo": "500 Commander Shea Boulevard Quincy, MA",
+        "Latitude": -22.906847,
+        "Longitude": -43.172896,
+        "Confidence": "High",
+        "AddyCheck": "500 Commander Shea Boulevard Quincy, MA"
+      }, "geometry": { "type": "Point", "coordinates": [ -43.172896, -22.906847 ] } }
+    ];
+
+    map = new google.maps.Map(document.getElementById('js-map'), mapOptions);
+
+    var myIcon ='assets/css/img/icon.png';
 
     //preparing the image so it can be used as a marker
     //https://developers.google.com/maps/documentation/javascript/reference#Icon
@@ -23,7 +52,6 @@ $(document).ready(function(){
       scaledSize: new google.maps.Size(70, 60),
       origin: new google.maps.Point(-15,0)
     }
-
     /*
     //If you want to do this without wobble animation no need for hacky fix
     var catIcon = {
@@ -33,51 +61,35 @@ $(document).ready(function(){
       origin: new google.maps.Point(0,0)
     }*/
 
-    //gets GeoJSON from external file
-    $.getJSON( "http://codepen.io/kevinkononenko/pen/dMKzgG.js", function(data) {
-        var length= data.features.length
-        for (var i=0; i < length; i++){
-          var eachData = data.features[i].properties
-          var latLng = new google.maps.LatLng(eachData.Latitude, eachData.Longitude);
-          var marker = new google.maps.Marker({
-              position:latLng,
-              map: map,
-              // set the icon as catIcon declared above
-              icon: catIcon,
-              // must use optimized false for CSS
-              optimized: false,
-              title: markerArr.length.toString()
-          });
-          markerArr.push(marker);
-
-          //add a click handler that does nothing at the moment
-          google.maps.event.addListener(marker, 'click', function() {
-            var thisTitle= Number(this.title);
-            //$('#markerLayer img').eq(thisTitle)
-          })
-
-        }
+    for (var i=0, length = features.length; i < length; i++){
+      var eachData = features[i].properties
+      var latLng = new google.maps.LatLng(eachData.Latitude, eachData.Longitude);
+      var marker = new google.maps.Marker({
+          position:latLng,
+          map: map,
+          // set the icon as catIcon declared above
+          icon: catIcon,
+          // must use optimized false for CSS
+          optimized: false,
+          title: markerArr.length.toString()
       });
+      markerArr.push(marker);
+      //add a click handler that does nothing at the moment
+      google.maps.event.addListener(marker, 'click', function() {
+        var thisTitle= Number(this.title);
+        //$('#markerLayer img').eq(thisTitle)
+      })
+    }
 
-       // Overlay view allows you to organize your markers in the DOM
-        // https://developers.google.com/maps/documentation/javascript/reference#OverlayView
-       var myoverlay = new google.maps.OverlayView();
-       myoverlay.draw = function () {
-           // add an id to the layer that includes all the markers so you can use it in CSS
-           this.getPanes().markerLayer.id='markerLayer';
-       };
-       myoverlay.setMap(map);
-
+    // Overlay view allows you to organize your markers in the DOM
+    // https://developers.google.com/maps/documentation/javascript/reference#OverlayView
+    var myoverlay = new google.maps.OverlayView();
+    myoverlay.draw = function () {
+      // add an id to the layer that includes all the markers so you can use it in CSS
+      this.getPanes().markerLayer.id='markerLayer';
+    };
+    myoverlay.setMap(map);
   }
 
   initialize();
-
-
-  // use jQuery to change the markers animation based on toggle button
-  $('.btn').click(function(){
-    var type = $(this).data('anim');
-    $('#markerLayer img').css('animation', type+ ' 1s infinite alternate');
-    $('#markerLayer img').css('-webkit-animation', type+ ' 1s infinite alternate')
-  })
-
 });
