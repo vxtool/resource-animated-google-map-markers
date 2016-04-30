0e17e21f1c5f1b17344cada6b74a5139db25c734
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-04-30 14:31:59 -0300

    update: task js

--- source/assets/js/script.js
+++ source/assets/js/script.js
@@ -1,5 +1,26 @@
-$(function(){
+(function(exports, d) {
+  function domReady(fn, context) {
 
+    function onReady(event) {
+      d.removeEventListener("DOMContentLoaded", onReady);
+      fn.call(context || exports, event);
+    }
+
+    function onReadyIe(event) {
+      if (d.readyState === "complete") {
+        d.detachEvent("onreadystatechange", onReadyIe);
+        fn.call(context || exports, event);
+      }
+    }
+
+    d.addEventListener && d.addEventListener("DOMContentLoaded", onReady) ||
+    d.attachEvent      && d.attachEvent("onreadystatechange", onReadyIe);
+  }
+
+  exports.domReady = domReady;
+})(window, document);
+
+domReady(function(event) {
   var map;
   var markerArr = [];
 
@@ -12,17 +33,17 @@ $(function(){
 
     var features = [
       { "type": "Feature", "properties": {
-        "Company": "Boston Scientific",
+        "Company": null,
         "StoreID": null,
-        "Site_Store_Facility Name": "Boston Scientific - Quincy Location",
+        "Site_Store_Facility Name": null,
         "Type of Facility": null,
         "Fac_simple": null,
-        "Street Address": "500 Commander Shea Boulevard",
-        "City": "Quincy",
-        "State": "MA",
-        "Zip": "2171",
+        "Street Address": null,
+        "City": "Rio de Janeiro",
+        "State": "RJ",
+        "Zip": null,
         "OnlineDate": null,
-        "Capacity": "1200",
+        "Capacity": null,
         "System Mounting": null,
         "NEM": null,
         "Solar Installer Company": null,
@@ -31,35 +52,24 @@ $(function(){
         "Facility Tenant": null,
         "Notes": null,
         "New": 0,
-        "LocationGeo": "500 Commander Shea Boulevard Quincy, MA",
+        "LocationGeo": null,
         "Latitude": -22.906847,
         "Longitude": -43.172896,
         "Confidence": "High",
-        "AddyCheck": "500 Commander Shea Boulevard Quincy, MA"
+        "AddyCheck": null
       }, "geometry": { "type": "Point", "coordinates": [ -43.172896, -22.906847 ] } }
     ];
 
     map = new google.maps.Map(document.getElementById('js-map'), mapOptions);
 
     var myIcon ='assets/css/img/icon.png';
-
-    //preparing the image so it can be used as a marker
     //https://developers.google.com/maps/documentation/javascript/reference#Icon
-    //includes hacky fix "size" to allow for wobble
     var catIcon = {
       url: myIcon,
       size: new google.maps.Size(100, 60),
       scaledSize: new google.maps.Size(70, 60),
       origin: new google.maps.Point(-15,0)
-    }
-    /*
-    //If you want to do this without wobble animation no need for hacky fix
-    var catIcon = {
-      url: myIcon,
-      size: new google.maps.Size(70, 60),
-      scaledSize: new google.maps.Size(70, 60),
-      origin: new google.maps.Point(0,0)
-    }*/
+    };
 
     for (var i=0, length = features.length; i < length; i++){
       var eachData = features[i].properties
@@ -67,26 +77,19 @@ $(function(){
       var marker = new google.maps.Marker({
           position:latLng,
           map: map,
-          // set the icon as catIcon declared above
           icon: catIcon,
-          // must use optimized false for CSS
           optimized: false,
           title: markerArr.length.toString()
       });
       markerArr.push(marker);
-      //add a click handler that does nothing at the moment
       google.maps.event.addListener(marker, 'click', function() {
         var thisTitle= Number(this.title);
-        //$('#markerLayer img').eq(thisTitle)
       })
     }
-
-    // Overlay view allows you to organize your markers in the DOM
     // https://developers.google.com/maps/documentation/javascript/reference#OverlayView
     var myoverlay = new google.maps.OverlayView();
     myoverlay.draw = function () {
-      // add an id to the layer that includes all the markers so you can use it in CSS
-      this.getPanes().markerLayer.id='markerLayer';
+      this.getPanes().markerLayer.id = 'target';
     };
     myoverlay.setMap(map);
   }
--- source/tasks/uglify.js
+++ source/tasks/uglify.js
@@ -14,7 +14,6 @@ module.exports = function(grunt, options){
       files: {
         '<%= projectDir %>js/script.min.js':
         [
-        '<%= projectVendor %>jquery/dist/jquery.min.js',
         '<%= projectDev %>js/maps.js',
         '<%= projectDev %>js/script.js'
         ]
